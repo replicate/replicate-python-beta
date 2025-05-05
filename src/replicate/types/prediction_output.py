1804f7c0e9f87bb53a1c126ffd1df5ab2c521cb9
David Meadows <dmeadows@stainless.com>
2025-05-05 18:01:20 -0400

    temp fix to generated type

--- src/replicate/types/prediction_output.py
+++ src/replicate/types/prediction_output.py
@@ -5,6 +5,7 @@ from typing_extensions import TypeAlias
 
 __all__ = ["PredictionOutput"]
 
+# todo: this shouldn't need to be custom code. We should update the spec to include the `Optional[List[str]]` type
 PredictionOutput: TypeAlias = Union[
-    Optional[Dict[str, object]], Optional[List[Dict[str, object]]], Optional[str], Optional[float], Optional[bool]
+    Optional[Dict[str, object]], Optional[List[Dict[str, object]]], Optional[List[str]], Optional[str], Optional[float], Optional[bool]
 ]
