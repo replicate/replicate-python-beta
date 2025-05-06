68242514bf7624f1e1e5d0e98d70ce7189c557b9
David Meadows <dmeadows@stainless.com>
2025-05-06 10:24:08 -0400

    adjust typing to just be Any for PredictionOutput

--- src/replicate/types/prediction_output.py
+++ src/replicate/types/prediction_output.py
@@ -1,16 +1,9 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-from typing import Dict, List, Union, Optional
+from typing import Any
 from typing_extensions import TypeAlias
 
 __all__ = ["PredictionOutput"]
 
-# todo: this shouldn't need to be custom code. We should update the spec to include the `Optional[List[str]]` type
-PredictionOutput: TypeAlias = Union[
-    Optional[Dict[str, object]],
-    Optional[List[Dict[str, object]]],
-    Optional[List[str]],
-    Optional[str],
-    Optional[float],
-    Optional[bool],
-]
+# todo: this shouldn't need to be custom code. We should update the spec to just be {type: object}
+PredictionOutput: TypeAlias = Any
